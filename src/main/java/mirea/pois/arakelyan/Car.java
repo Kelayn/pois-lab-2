251bf2bc3eb0f8feec8f34acf407b427722df11d
Andrew <andrey.arakelyan@bask.ws>
2020-10-29 20:15:58 +0300

    Добавил систему лабы

---
+++ src/main/java/mirea/pois/arakelyan/Car.java
@@ -0,0 +1,36 @@
+package mirea.pois.arakelyan;
+
+public class Car {
+    private Owner owner;
+    private Integer allowedOwners;
+    private Integer hp;
+
+    public Car (Owner owner, Integer allowedOwners, Integer hp){
+        this.owner = owner;
+        this.allowedOwners = allowedOwners;
+        this.hp = hp;
+    }
+
+    public Owner getOwner() {
+        return owner;
+    }
+
+    public Integer getAllowedOwners() {
+        return allowedOwners;
+    }
+
+    public Integer getHp() {
+        return hp;
+    }
+
+    public void setHp(Integer hp) {
+        this.hp = hp;
+    }
+
+
+    /*
+    not creating setters,
+    assuming that data will be pulled from db or mocked
+     */
+
+}
